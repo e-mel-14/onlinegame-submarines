5f878f57521504d55eaf986c9d82baf307365cba
melissa <e-panjaitan@xware.co.jp>
2020-06-01 15:27:00 +0900

    many files

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  twitter: {
+    consumerKey: '6xLbbHK2hiVfjiA073HPuT2nx',
+    consumerSecret: 'Kb13mKdbQ4w5Mi4DdVYgNdpSIbJE07EgyBdeYtHQrQdcbr4HHE',
+    callbackURL: 'http://localhost:8000/oauth_callback'
+  },
+  ipAddress: 'http://localhost:8000'
+};
